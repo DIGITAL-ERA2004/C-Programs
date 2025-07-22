e06e239be9655382958e0970f8686586beb71f7b
Kantrao Datta Paratkar <191096887+DIGITAL-ERA2004@users.noreply.github.com>
2025-07-22 16:16:15 +0530

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+ int main()
+{
+  printf("hello world\n");
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <stdio.h>
+
+int main() 
+{
+  printf("Hello World\n");
+  return 0;
+}*/
